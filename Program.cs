c1381582cf7f33dbca57dae732479fa315a157fa
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 19:49:21 -0500

    Initialization and creation of tostring method

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+public class Polynominal
+{
+    private double[] _coefficients;
+
+    public Polynominal(params double[] coefficients)
+    {
+        _coefficients = coefficients;
+    }
+
+    public override string ToString()
+    {
+        var stringBuilder = new StringBuilder();
+        for (int i = _coefficients.Length - 1; i >= 0; i--)
+        {
+            if (_coefficients[i] == 0)
+                continue;
+            if (stringBuilder.Length > 0 && _coefficients[i] > 0)
+                stringBuilder.Append('+');
+        }
+    }
+}
